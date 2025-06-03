32cd590667a186414e8137d26129e82ae623b4b0
Venkannababu Kothapalli <161434793+Venky-43@users.noreply.github.com>
2025-06-03 14:42:03 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-# face_app/forms.py
-from django import forms
-from .models import UserProfile
-
-class RegisterForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['username', 'email', 'face_image']
